151e571237f78526030c64908be2a72c85c62b40
skmm6 <egor.qaq@gmail.com>
2023-01-31 14:25:33 +0300

    31.01.23

--- assets/js/main.js
+++ assets/js/main.js
@@ -4,14 +4,70 @@ const containerContent = document.querySelector(".app-content");
 const homeImg = document.querySelector(".app-img");
 const homePage = document.querySelector(".home-page");
 const constructorPage1 = document.querySelector(".constructor-page-1");
+const footer = document.querySelector(".footer");
 
 const btnOpenConstructor = document.getElementById("open-constructor");
 
+// стейт для страниц
+let pageNumber = 2;
+
 btnOpenConstructor.addEventListener("click", () => {
+  pageNumber++;
+  pagesNumber();
+});
+
+if (pageNumber > 0) {
   app.classList.add("open");
   containerContent.classList.remove("container-home");
   containerContent.classList.add("container-constructor");
   homeImg.classList.add("close");
-  homePage.classList.add("close");
-  constructorPage1.classList.add("open");
+  // homePage.classList.add("close");
+  // constructorPage1.classList.add("open");
+  footer.classList.add("footer-constructor");
+}
+
+// =======================OPEN CONST=================//
+
+// =======================КОНСТРУКТОР СЛАЙДЕР СТРАНИЦ=================//
+
+const pages = document.querySelectorAll(".pages");
+console.log(pages);
+
+const pagesNumber = function () {
+  pages.forEach((page, id) => {
+    if (pageNumber === id) {
+      page.classList.add("open");
+    } else {
+      page.classList.remove("open");
+    }
+  });
+};
+// const кнопок
+const btnsNext = document.querySelectorAll(".next");
+const btnsPrev = document.querySelectorAll(".prev");
+
+// для кнопки продолжить
+btnsNext.forEach((btn) => {
+  btn.addEventListener("click", () => {
+    if (pageNumber < pages.length - 1) {
+      pageNumber++;
+      pagesNumber();
+      console.log(pageNumber);
+    }
+  });
 });
+
+// для кнопки назад
+btnsPrev.forEach((btn) => {
+  btn.addEventListener("click", () => {
+    if (pageNumber > 0) {
+      pageNumber--;
+      pagesNumber();
+    }
+  });
+});
+
+pagesNumber();
+// pages[i].classList.add("open");
+
+// =======================КОНСТРУКТОР СЛАЙДЕР СТРАНИЦ=================//
